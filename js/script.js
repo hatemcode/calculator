f00473765f7f3a97b6b21a5483898abe015cd9eb
Hatem A <hatemcode@users.noreply.github.com>
2016-09-09 13:25:24 +0400

    Initial code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-
+/**
+ * Calculator
+ * @author Hatem A. <hatem@tuta.io>
+ * @version 1.0
+ * @licence Licensed under the Apache License, Version 2.0
+ */
+ 
+$(document).ready(function(){
+  calculator.init();
+});
